f672a5d6daf5e4dc5f799d20811a392790b3bf5c
Mohamad Jahani <m4m4lj@gmail.com>
2018-11-22 17:18:25 +0330

    refactor: make login form address a variable
    and also, move deferred request body close to the  right position

--- main.go
+++ main.go
@@ -10,14 +10,11 @@ import (
 )
 
 var (
-	reyhoonHotspotIP       string
-	reyhoonHotspotUsername string
-	reyhoonHotspotPassword string
+	reyhoonHotspotIP               string
+	reyhoonHotspotUsername         string
+	reyhoonHotspotPassword         string
+	reyhoonHotspotLoginFormAddress string
 )
-
-func getLoginFormAddress() string {
-	return fmt.Sprintf("http://%s/login", reyhoonHotspotIP)
-}
 
 func init() {
 	reyhoonHotspotIP = os.Getenv("RHN_HS_IP")
@@ -28,11 +25,11 @@ func init() {
 		beeep.Notify("Oops!", "Please set all RHN_HS environment variables!", "")
 		os.Exit(1)
 	}
+	reyhoonHotspotLoginFormAddress = fmt.Sprintf("http://%s/login", reyhoonHotspotIP)
 }
 
 func main() {
-	address := getLoginFormAddress()
-	response, err := http.PostForm(address, url.Values{
+	response, err := http.PostForm(reyhoonHotspotLoginFormAddress, url.Values{
 		"username": {reyhoonHotspotUsername},
 		"password": {reyhoonHotspotPassword},
 	})
@@ -41,7 +38,7 @@ func main() {
 		beeep.Notify("Oops!", "Error sending post request to login form!", "")
 		os.Exit(1)
 	}
+	defer response.Body.Close()
 	beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
 	beeep.Notify("Done!", "Successfully logged in to Reyhoon hotspot!", "")
-	defer response.Body.Close()
 }
